a3b00df21eabdcd6aaa61f92fb1e6ea8d38721e4
hanxiaoshun <1425814646@qq.com>
2018-04-11 19:07:36 +0800

    添加了一些基础知识

---
+++ asyncTest/coroutineTest.py
@@ -0,0 +1,39 @@
+def A():
+    print('1')
+    print('2')
+    print('3')
+
+def B():
+    print('x')
+    print('y')
+    print('z')
+
+# print(A(),B())
+# 1
+# 2
+# 3
+# x
+# y
+# z
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None) #启动生成器
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
