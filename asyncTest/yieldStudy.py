2c142e01ba57339892c547099ce64f721e35146e
hanxiaoshun <1425814646@qq.com>
2018-04-13 17:48:35 +0800

    不仅学习了生成器概念吗，更重要的是对对象的引用和复制的深入理解。

---
+++ asyncTest/yieldStudy.py
@@ -0,0 +1,60 @@
+def fibg(max):
+    n , a, b = 0,0,1
+    while n<max:
+        yield b
+        a,b = b,a+b
+        n = n+1
+    return 'done'
+
+# for i in fibg(5):
+#     print(i)
+
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield 3
+    print('step 3')
+    yield 5
+
+# o = odd()
+# print(next(o))
+# print(next(o))
+# print(next(o))
+# print(next(o))
+
+# g = fibg(5)
+# while True:
+#     try:
+#         x = next(g)
+#         print('g;',x)
+#     except StopIteration as e:
+#         print('Generator return value:' ,e.value)
+#         break
+
+
+def yanghui(max):
+    t = [1]
+    n = 1
+    while n<max:
+        print(t)
+        if n>0:
+            L = t.copy()
+            for i in range(1,len(t)):
+                L[i] = t[i]+t[i-1]
+            L.append(1)
+            # print(L)
+        t = L
+        n= n+1
+print(yanghui(7))
+print('-------------------------')
+for i in range(2):
+    print(i)
+
+
+
+
+
+
+
