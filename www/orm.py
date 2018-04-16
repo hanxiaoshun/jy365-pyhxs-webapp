cc93391a8ed2bb1ee06de58fffaed3f8414cb064
hanxiaoshun <1425814646@qq.com>
2018-04-16 17:15:29 +0800

    complated task of edite orm.py file

--- www/orm.py
+++ www/orm.py
@@ -12,7 +12,7 @@ async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
+        host=kw.get('host', '60.205.228.47'),
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
