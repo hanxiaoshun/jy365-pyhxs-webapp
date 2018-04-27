aa1226ea2d9bfc6a5df78de38221dae80a618075
Shun <18301513217>
2018-04-27 23:53:57 +0800

    update handlers htmi

--- www/handlers.py
+++ www/handlers.py
@@ -39,3 +39,29 @@ def responsive(request):
         '__template__': 'responsive.html'
         # 'blogs': responsive
     }
+@get('/base')
+def base(request):
+    # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    # blogs = [
+    #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+    #     Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+    #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    # ]
+    return {
+        '__template__': 'base.html'
+        # 'blogs': responsive
+    }
+
+
+@get('/suggest')
+def suggest(request):
+    # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    # blogs = [
+    #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+    #     Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+    #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    # ]
+    return {
+        '__template__': 'suggest.html'
+        # 'blogs': responsive
+    }
