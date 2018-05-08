ab5062280a2a065622b42190419c643b7f50c35d
Shun <18301513217>
2018-05-08 22:33:17 +0800

    add many handlers

--- www/handlers.py
+++ www/handlers.py
@@ -65,3 +65,15 @@ def suggest(request):
         '__template__': 'suggest.html'
         # 'blogs': responsive
     }
+@get('/urls')
+def urls(request):
+    # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    # blogs = [
+    #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+    #     Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+    #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    # ]
+    return {
+        '__template__': 'urls.html'
+        # 'blogs': responsive
+    }
