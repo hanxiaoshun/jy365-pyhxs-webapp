333aba87b7e21766bd7fb7e9e8fe34fca749b6e4
Shun <18301513217>
2018-05-12 17:32:56 +0800

    update handlers file

--- www/handlers.py
+++ www/handlers.py
@@ -9,11 +9,12 @@ __author__ = 'Michael Liao'
 #
 # ' url handlers '
 
-import re, time, json, logging, hashlib,base64,asyncio
+import re, time, json, logging, hashlib, base64, asyncio
 
 from coroweb import get, post
 
-from models import User,Comment, Blog,next_id
+from models import User, Comment, Blog, next_id
+
 
 @get('/')
 def index(request):
@@ -27,6 +28,7 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
 @get('/responsive')
 def responsive(request):
     # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -39,6 +41,8 @@ def responsive(request):
         '__template__': 'responsive.html'
         # 'blogs': responsive
     }
+
+
 @get('/base')
 def base(request):
     # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -65,8 +69,9 @@ def suggest(request):
         '__template__': 'suggest.html'
         # 'blogs': responsive
     }
-@get('/urls')
-def urls(request):
+
+@get('/favicon.ico')
+def favicon(request):
     # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     # blogs = [
     #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
@@ -74,6 +79,21 @@ def urls(request):
     #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
     # ]
     return {
-        '__template__': 'urls.html'
+        '__template__': '/static/favicon.ico'
         # 'blogs': responsive
     }
+
+@get('/favicon.ico')
+def favicon(request):
+    # summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    # blogs = [
+    #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+    #     Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+    #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    # ]
+    return {
+        '__template__': '/static/favicon.ico'
+        # 'blogs': responsive
+    }
+
+
